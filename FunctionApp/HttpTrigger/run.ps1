6089d7eee5b3a17c78ce94c2d4991cbd19177190
unknown <conner.schiissler@gmail.com>
2020-10-08 19:08:30 -0600

    Edited run.ps1 to display sithlords.json data

--- FunctionApp/HttpTrigger/run.ps1
+++ FunctionApp/HttpTrigger/run.ps1
@@ -1,25 +1,11 @@
 using namespace System.Net
 
-# Input bindings are passed in via param block.
 param($Request, $TriggerMetadata)
 
-# Write to the Azure Functions log stream.
-Write-Host "PowerShell HTTP trigger function processed a request."
+$sithLords = Get-Content .\sithLords.json | ConvertFrom-Json
 
-# Interact with query parameters or the body of the request.
-$name = $Request.Query.Name
-if (-not $name) {
-    $name = $Request.Body.Name
-}
-
-$body = "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-
-if ($name) {
-    $body = "Hello, $name. This HTTP triggered function executed successfully."
-}
-
-# Associate values to output bindings by calling 'Push-OutputBinding'.
 Push-OutputBinding -Name Response -Value ([HttpResponseContext]@{
-    StatusCode = [HttpStatusCode]::OK
-    Body = $body
+    headers = @{'content-type'='application\json'}
+    StatusCode = [httpstatuscode]::OK
+    Body = $sithLords.sithLords
 })
