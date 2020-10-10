2c2fdb92b6d43bf2933e1a4ae00d094ec536a844
unknown <conner.schiissler@gmail.com>
2020-10-09 23:01:30 -0600

    Added script to apply security post deployment

---
+++ Infrastructure/setSecurity.ps1
@@ -0,0 +1,19 @@
+<#
+.SYNOPSIS
+Sets security on various resources post deployment
+.AUTHOR
+nullptr
+#>
+
+Select-AzSubscription -SubscriptionId c2c6e684-985c-4a2e-98ea-128c6d48609b
+
+$faObject = Get-AzFunctionApp -Name functionappcicd98765 -ResourceGroupName connerDevOps
+
+$msiId = $faObject.IdentityPrincipalId
+
+$msiId
+
+Set-AzKeyVaultAccessPolicy -VaultName connerCiCD `
+                           -ObjectId $msiId `
+                           -PermissionsToSecrets get,list `
+                           -PassThru
