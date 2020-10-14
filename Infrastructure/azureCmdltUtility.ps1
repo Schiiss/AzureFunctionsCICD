aeee8db24a71528557fed99a34bcae18389dc9f2
unknown <conner.schiissler@gmail.com>
2020-10-13 18:35:13 -0600

    Added azure cmdlt utility to start to clean up powershell code

---
+++ Infrastructure/azureCmdltUtility.ps1
@@ -0,0 +1,37 @@
+<#
+.SYNOPSIS
+This utility file contains classes and methods that can be called to interact with azure resources
+
+.AUTHOR
+nullptr
+#>
+
+class Auth {
+    [string]$subId
+
+    Auth([string]$subId) {
+        $this.subId = $subId
+    }
+
+    [object]Login() {
+        $subscription = Select-AzSubscription -SubscriptionId $this.subId
+        return $subscription
+    }
+
+    [object]CheckSubscriptionContext() {
+        $subscriptionContext = Get-AzContext
+        return $subscriptionContext
+    }   
+}
+
+class FunctionApps {
+    [object]GetFunctionApp([string]$name, [string]$resourceGroup){
+       $faObject = Get-AzFunctionApp -Name $name -ResourceGroupName $resourceGroup
+       return $faObject
+    }
+
+}
+
+class KeyVaults {
+
+}
--- Infrastructure/setSecurity.ps1
+++ Infrastructure/setSecurity.ps1
@@ -4,10 +4,30 @@ Sets security on various resources post deployment
 .AUTHOR
 nullptr
 #>
+[cmdletbinding(SupportsShouldProcess = $true)]
+param(
+    [string]$subId = "c774d2e1-02de-43c2-a9c7-3c1ba6372734"
+)
 
-Select-AzSubscription -SubscriptionId c2c6e684-985c-4a2e-98ea-128c6d48609b
+. "$PSScriptRoot.\azureCmdltUtility.ps1"
 
-$faObject = Get-AzFunctionApp -Name functionappcicd98765 -ResourceGroupName connerDevOps
+$authHelper = [Auth]::new($subId)
+$functionAppHelper = [FunctionApps]::new()
+
+Write-Host "Checking Subscription"
+$checkSubId = $authHelper.CheckSubscriptionContext()
+$validatedSubId = $checkSubId.Subscription.Id
+if ($validatedSubId -ne $subId) {
+    Write-Output ("Wrong Subscription: {0}" -f $validatedSubId)
+    Write-Output ("Logging you into: {0}" -f $subId)
+    $authHelper.Login()
+    $authHelper.CheckSubscriptionContext()
+}
+else {
+    Write-Output ("Correct Subscription: {0}" -f $validatedSubId)
+}
+
+$faObject = $functionAppHelper.GetFunctionApp("functionappcicd98765", "connerDevOps")
 
 $msiId = $faObject.IdentityPrincipalId
 
