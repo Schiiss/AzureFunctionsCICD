0840d724db1e9ed8ab72d50a6cd0266582fdeb46
unknown <conner.schiissler@gmail.com>
2020-10-13 18:47:03 -0600

    reverted script change for testing

--- Infrastructure/setSecurity.ps1
+++ Infrastructure/setSecurity.ps1
@@ -1,33 +1,6 @@
-<#
-.SYNOPSIS
-Sets security on various resources post deployment
-.AUTHOR
-nullptr
-#>
-[cmdletbinding(SupportsShouldProcess = $true)]
-param(
-    [string]$subId = "c2c6e684-985c-4a2e-98ea-128c6d48609b"
-)
+Select-AzSubscription -SubscriptionId c2c6e684-985c-4a2e-98ea-128c6d48609b
 
-. "$PSScriptRoot.\azureCmdltUtility.ps1"
-
-$authHelper = [Auth]::new($subId)
-$functionAppHelper = [FunctionApps]::new()
-
-Write-Host "Checking Subscription"
-$checkSubId = $authHelper.CheckSubscriptionContext()
-$validatedSubId = $checkSubId.Subscription.Id
-if ($validatedSubId -ne $subId) {
-    Write-Output ("Wrong Subscription: {0}" -f $validatedSubId)
-    Write-Output ("Logging you into: {0}" -f $subId)
-    $authHelper.Login()
-    $authHelper.CheckSubscriptionContext()
-}
-else {
-    Write-Output ("Correct Subscription: {0}" -f $validatedSubId)
-}
-
-$faObject = $functionAppHelper.GetFunctionApp("functionappcicd98765", "connerDevOps")
+$faObject = Get-AzFunctionApp -Name functionappcicd98765 -ResourceGroupName connerDevOps
 
 $msiId = $faObject.IdentityPrincipalId
 
