19f2491da511137bc2bd21447b3fb7d82f10c221
unknown <conner.schiissler@gmail.com>
2020-10-13 18:52:05 -0600

    Added azure auth helper back to test

--- Infrastructure/setSecurity.ps1
+++ Infrastructure/setSecurity.ps1
@@ -1,4 +1,27 @@
-Select-AzSubscription -SubscriptionId c2c6e684-985c-4a2e-98ea-128c6d48609b
+<#
+.AUTHOR
+nullptr
+#>
+param(
+    [string]$subId = "c2c6e684-985c-4a2e-98ea-128c6d48609b"
+)
+
+. "$PSScriptRoot.\azureCmdltUtility.ps1"
+
+$authHelper = [Auth]::new($subId)
+
+Write-Host "Checking Subscription"
+$checkSubId = $authHelper.CheckSubscriptionContext()
+$validatedSubId = $checkSubId.Subscription.Id
+if ($validatedSubId -ne $subId) {
+    Write-Output ("Wrong Subscription: {0}" -f $validatedSubId)
+    Write-Output ("Logging you into: {0}" -f $subId)
+    $authHelper.Login()
+    $authHelper.CheckSubscriptionContext()
+}
+else {
+    Write-Output ("Correct Subscription: {0}" -f $validatedSubId)
+}
 
 $faObject = Get-AzFunctionApp -Name functionappcicd98765 -ResourceGroupName connerDevOps
 
