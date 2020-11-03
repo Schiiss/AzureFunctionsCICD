dc6be69c8ff793d9d98ceca8e13c969fdb808311
unknown <conner.schiissler@gmail.com>
2020-11-02 19:45:54 -0700

    User can now search for a cluster by name and deploy a cluster. Lots of hard coded variables right now that need to be fixed

--- FunctionApp/DatabricksClusters/run.ps1
+++ FunctionApp/DatabricksClusters/run.ps1
@@ -5,21 +5,40 @@ using module ./Modules/databricksUtility.psm1
 param($Request, $TriggerMetadata)
 
 $databricksHelper = [DatabricksCLI]::new($env:DatabricksURL,$env:DatabricksPAT)
+$clusterConfig = [ClusterUtility]::GetClusterConfig("connerTempTest")
 $httpMethod = $Request.Method
-$clusters = $databricksHelper.ListClusters()
 
-
-switch($httpMethod){
-    "GET"{
+switch ($httpMethod) {
+    "GET" {
+        $name = $Request.Query.Name
+        if (-not $name) {
+            $name = $Request.Body.Name
+        }
+        
+        Write-Information ("Getting Cluster Specs For: {0}" -f $name)
+        $body = $databricksHelper.GetClusterByName($name)
         Push-OutputBinding -Name response -Value ([HttpResponseContext]@{
-            StatusCode = 200
-            Body       = $clusters
-        })
+                StatusCode  = 200
+                ContentType = "application/json; charset=utf-8"
+                Body        = $body
+            })
+        return
+        
     }
-    "POST"{
-        Push-OutputBinding -Name response -Value ([HttpResponseContext]@{
-            StatusCode = 200
-            Body       = "test"
-        })
+    "POST" {
+        Write-Information ("Deploying Cluster for {0}" -f ($clusterConfig | ConvertTo-Json | Out-String))
+        try {
+            $databricksHelper.CreateCluster($clusterConfig)
+        }
+        catch {
+            Write-Error $_
+            Push-OutputBinding -Name Response -Value ([HttpResponseContext]@{
+                StatusCode = 500
+                ContentType = "application/json; charset=utf-8"
+                Body = "Could not create cluster"
+            })
+            return
+        }
     }
 }
+
--- FunctionApp/Modules/databricksUtility.psm1
+++ FunctionApp/Modules/databricksUtility.psm1
@@ -27,13 +27,19 @@ class DatabricksCLI {
         return $this.ExecuteAPICall($uri, "Get")
     }
 
-    [void]CreateCluster([object]$clusterConfig){
-        Write-Verbose ("Creating cluster {0}" -f $clusterConfig)
-        $uri = "https://{0}/api/2.0/clusters/create" -f $this.url
-        $this.ExecuteAPICall($uri, "Post", ($clusterConfig | ConvertFrom-Json))
+    [object]GetClusterByName([string]$clusterName) {
+        Write-Verbose ("Getting info for cluster {0}" -f $clusterName)
+        $clusters = $this.ListClusters()
+        return $clusters.clusters.Where( { $_.cluster_name -eq $clusterName })[0]
     }
 
-    [object]ExecuteAPICall([string]$uri,[string]$method,[string]$body){
+    [void]CreateCluster([object]$clusterConfig) {
+        Write-Verbose ("Creating cluster {0}" -f $clusterConfig)
+        $uri = "https://{0}/api/2.0/clusters/create" -f $this.url
+        $this.ExecuteAPICall($uri, "Post", ($clusterConfig | ConvertTo-Json))
+    }
+
+    [object]ExecuteAPICall([string]$uri, [string]$method, [string]$body) {
         Write-Information ("Body: " -f ($body | Out-String))
         $response = Invoke-WebRequest -Method $method -Uri $uri -Headers $this.GetHeaders() -Body $body -Verbose
         Write-Information ("RESPONSE: {0}" -f $response)
@@ -51,15 +57,13 @@ class ClusterUtility {
 
     static [object]GetClusterConfig([string]$clusterName) {
         $clusterConfig = @{
-            cluster_name            = $clusterName
-            spark_version           = "latest-stable-scala2.11"
-            enable_elastic_disk     = $true
-            autotermination_minutes = 10
-            num_workers             = 1
-            spark_env_vars          = @{
-                PYSPARK_PYTHON = "/databricks/python3/bin/python3"
+            cluster_name  = $clusterName
+            spark_version = "latest-stable-scala2.11"
+            node_type_id  = "Standard_D3_v2"
+            autoscale     = @{
+                min_workers = 2
+                max_workers = 3
             }
-            node_type_id            = "Standard_D3_v2"
           
         }
 
